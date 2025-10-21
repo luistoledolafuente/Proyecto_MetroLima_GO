333af72e98a833378280c5913e890961eaefb466
Carlos Valeriano <carlos.valeriano@tecsup.edu.pe>
2025-10-20 20:13:38 -0500

    3eri dia 2.0

--- app/src/main/java/com/metrolimago/data/repository/MetroRepository.kt
+++ app/src/main/java/com/metrolimago/data/repository/MetroRepository.kt
@@ -1,2 +1,7 @@
 package com.metrolimago.data.repository
 
+import com.metrolimago.data.dao.EstacionDao
+
+class MetroRepository(private val estacionDao: EstacionDao) {
+    val todasLasEstaciones = estacionDao.obtenerTodas()
+}
